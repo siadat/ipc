5c317390b134ddc752376204f51b49b3941c6bcb
sina <siadat@gmail.com>
2020-02-15 23:33:43 +0330

    msgctl: return error if cmd != IPC_RMID

--- msgctl.go
+++ msgctl.go
@@ -1,12 +1,16 @@
 package ipc
 
 import (
+	"fmt"
 	"syscall"
 )
 
 // Msgctl calls the msgctl() syscall.
 // FIXME: we are not passing the buf argument, see msgctl(2).
 func Msgctl(qid uint, cmd int) error {
+	if cmd != IPC_RMID {
+		return fmt.Errorf("only IPC_RMID is supported at the moment")
+	}
 	var buf uintptr = 0
 	_, _, err := syscall.Syscall(syscall.SYS_MSGCTL, uintptr(qid), buf, 0)
 	if err != 0 {
