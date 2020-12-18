3523b28df2e8b286a15ee68d6195cfaf80c4b81c
peano88 <ilpeano@gmail.com>
2020-12-18 15:59:48 +0000

    clean up

--- msgsnd.go
+++ msgsnd.go
@@ -18,39 +18,6 @@ func Msgsnd(qid uint, msg *Msgbuf, flags uint) error {
 	return MsgsndPrepared(qid, len(msg.Mtext), buffer, flags)
 }
 
-
-// 	if len(msg.Mtext) > msgmax {
-// 		return fmt.Errorf("mtext is too large, %d > %d", len(msg.Mtext), msgmax)
-// 	}
-
-// 	buf := make([]byte, uintSize+msgmax)
-// 	buffer := bytes.NewBuffer(buf)
-// 	buffer.Reset()
-// 	var err error
-// 	switch uintSize {
-// 	case 4:
-// 		err = binary.Write(buffer, binary.LittleEndian, uint32(msg.Mtype))
-// 	case 8:
-// 		err = binary.Write(buffer, binary.LittleEndian, uint64(msg.Mtype))
-// 	}
-// 	if err != nil {
-// 		return fmt.Errorf("Can't write binary: %v", err)
-// 	}
-// 	buffer.Write(msg.Mtext)
-// 	_, _, errno := syscall.Syscall6(syscall.SYS_MSGSND,
-// 		uintptr(qid),
-// 		uintptr(unsafe.Pointer(&buf[0])),
-// 		uintptr(len(msg.Mtext)),
-// 		uintptr(flags),
-// 		0,
-// 		0,
-// 	)
-// 	if errno != 0 {
-// 		return errno
-// 	}
-// 	return nil
-// }
-
 // PrepareMsg creates a buffer containing the message to send
 func PrepareMsg(msg *Msgbuf) (*bytes.Buffer, error) {
 	if len(msg.Mtext) > msgmax {
